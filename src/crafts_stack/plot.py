256ba94d340f56c430e4e6ca99a38c0391f81d86
dyliu0312 <liudy0312@gmail.com>
2026-01-16 20:57:59 +0800

    update: add multiple hpfunc kwargs support

--- src/crafts_stack/plot.py
+++ src/crafts_stack/plot.py
@@ -32,15 +32,16 @@ def create_subplots(n=1, ncols=1, figsize=(12, 8), **kwargs):
 
     if n == 1:
         return fig, axes
-        
+
     # Flatten the axes array for easier iteration, regardless of shape
     axes = axes.flatten()
 
     # Turn off the visibility of unused axes
     for i in range(n, len(axes)):
-        axes[i].axis('off')
+        axes[i].axis("off")
 
     return fig, axes[:n]
+
 
 def plot_hp_grid(
     data,
@@ -63,26 +64,41 @@ def plot_hp_grid(
         A list/array of maps to plot.
     title : list or array, optional
         Titles for each subplot. Must have the same length as data if provided.
-    nrows : int
-        Number of rows in the grid. Default: 1.
-    ncols : int
-        Number of columns in the grid. Default: 1.
-    hp_func : function, optional
+    hpfunc : function, optional
         Healpy plotting function to use (default: hp.mollview).
+    hpfunc_kw : dict or list of dicts, optional
+        Extra keyword arguments for the Healpy plotting function. If a list is
+        passed, each dictionary is passed to the corresponding subplot.
+    ncol : int
+        Number of columns in the grid. Default: 1.
     figsize : tuple, optional
         Figure size in inches (default: (12, 8)).
-    subplots_kwargs : dict, optional
+    subplots_kw : dict, optional
         Extra keyword arguments for plt.subplots().
-    hp_kwargs : dict, optional
-        Extra keyword arguments for the Healpy plotting function.
+    return_pmap : bool, optional
+        Whether to return the projected map. Default: False.
+    graticule : bool, optional
+        Whether to plot graticule lines. Default: True.
+    graticule_kw : dict, optional
+        Extra keyword arguments for hp.graticule().
     """
     if isinstance(data, np.ma.MaskedArray) or isinstance(data, np.ndarray):
         data = [data]
     n = len(data)
     if subplots_kw is None:
         subplots_kw = {}
+
     if hpfunc_kw is None:
-        hpfunc_kw = {}
+        hpfunc_kw_list = [{}] * n
+    elif isinstance(hpfunc_kw, dict):
+        hpfunc_kw_list = [hpfunc_kw] * n
+    elif isinstance(hpfunc_kw, list):
+        if len(hpfunc_kw) != n:
+            raise ValueError("Length of hpfunc_kw must match length of data")
+        hpfunc_kw_list = hpfunc_kw
+    else:
+        raise TypeError("hpfunc_kw must be a dict or a list of dicts")
+
     if title is None:
         title = [None] * n
     elif isinstance(title, str) and n > 1:
@@ -101,16 +117,22 @@ def plot_hp_grid(
         plt.sca(axs)  # type: ignore # set current axis for healpy plotting
         sub_title = title[0]
         if return_pmap:
-            pm = hpfunc(data[0], hold=True, title=sub_title, return_projected_map=True,**hpfunc_kw)
+            pm = hpfunc(
+                data[0],
+                hold=True,
+                title=sub_title,
+                return_projected_map=True,
+                **hpfunc_kw_list[0],
+            )
             if graticule:
                 hp.graticule(**kw_graticule)
             return axs, pm
         else:
-            hpfunc(data[0], hold=True, title=sub_title, **hpfunc_kw)
+            hpfunc(data[0], hold=True, title=sub_title, **hpfunc_kw_list[0])
             if graticule:
                 hp.graticule(**kw_graticule)
             return axs
-    
+
     else:
         # Loop through data and plot
         pm_list = []
@@ -118,12 +140,18 @@ def plot_hp_grid(
             plt.sca(ax)  # set current axis for healpy plotting
             sub_title = title[idx]
             if return_pmap:
-                pm = hpfunc(data[idx], hold=True, title=sub_title, return_projected_map=True, **hpfunc_kw)
+                pm = hpfunc(
+                    data[idx],
+                    hold=True,
+                    title=sub_title,
+                    return_projected_map=True,
+                    **hpfunc_kw_list[idx],
+                )
                 pm_list.append(pm)
                 if graticule:
                     hp.graticule(**kw_graticule)
             else:
-                hpfunc(data[idx], hold=True, title=sub_title, **hpfunc_kw)
+                hpfunc(data[idx], hold=True, title=sub_title, **hpfunc_kw_list[idx])
                 if graticule:
                     hp.graticule(**kw_graticule)
         if return_pmap:
